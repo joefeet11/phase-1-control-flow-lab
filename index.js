a579a120f2fe8811538fea4a1d8e8a00be609bcb
Kevin Olivier <kevinolivier1111@gmail.com>
2022-09-19 16:25:44 -0600

    commit tree 4b915284cf8a60245e13695bb67a3ebcc5ff3802

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
+function scuberGreetingForFeet(someValue){
+  if (someValue <= 400) {
+    return 'This one is on me!';
+  } else if (someValue <= 2000) {
+    return 'That will be twenty bucks.';
+  } else if (someValue <= 2500) {
+    return 'I will gladly take your thirty bucks.';
+  }else if (someValue > 2500 ) {
+    return 'No can do.';
+  }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+};
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  if (city === "NYC"){
+    return "Ok, sounds good.";
+  } else {
+    return "No go.";
+  }
+};
+
+function switchOnCharmFromTip(tipValue){
+  switch (tipValue) {
+    case 'generous' :
+      return 'Thank you so much.';
+      break;
+    case 'not as generous' :
+      return 'Thank you.';
+      break;
+    default :
+      return 'Bye.';
+      break;
+  };
 }
